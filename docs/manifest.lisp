1c8d2118ef9eb4d8be1339468cb78c7aaca17e6d
ma haidong <13501108114@163.com>
2019-10-28 17:05:48 +0800

    init

--- docs/manifest.lisp
+++
@@ -1,7 +0,0 @@
-(:docstring-markup-format :scriba
- :systems (:caad4lisp)
- :documents ((:title "caad4lisp"
-              :authors ("mahaidong")
-              :output-format (:type :multi-html
-                              :template :minima)
-              :sources ("manual.scr") )))
